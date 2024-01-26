f1abdd2b434590998474f1523a3636b077a7ef43
Andreapiani95 <andreapiani1995@gmail.com>
2024-01-26 10:15:05 +0100

    Crud started

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Restaurant;
+use App\Models\Dish;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Restaurant\StoreRestaurantRequest;
+use App\Http\Requests\Restaurant\UpdateRestaurantRequest;
+
 use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
@@ -13,7 +19,9 @@ class RestaurantController extends Controller
      */
     public function index()
     {
+        $restaurant = Restaurant::all();
         //
+        return view('user.restaurant.index', compact('restaurants'));
     }
 
     /**
@@ -23,7 +31,9 @@ class RestaurantController extends Controller
      */
     public function create()
     {
+        $dishes = Dish::all();
         //
+        return view('user.restaurant.create', compact('dishes'));
     }
 
     /**
@@ -34,7 +44,46 @@ class RestaurantController extends Controller
      */
     public function store(Request $request)
     {
+        /*
+
+
+        if (isset($formData['photo'])) {
+            $photo_path = Storage::put('uploads/images', $formData['photo']);
+        }
+
+        if (isset($formData['cv'])) {
+            $cv_path = Storage::put('uploads/pdf', $formData['cv']);
+        }
+
+        $teacher = Teacher::create(
+        [
+            'user_id'=>$formData['user_id'],
+            'bio'=>  $formData['bio'],
+            'cv' =>  $cv_path,
+            'photo' =>  $photo_path,
+            'phone' =>  $formData['phone'],
+            'service' =>  $formData['service'],
+        ]);
+
+        if (isset($formData['subjects'])) {
+            foreach ($formData['subjects'] as $subjectId) {
+
+                $teacher->subjects()->attach($subjectId);
+            }
+        }
+
+        $user = User::find(auth()->user()->id);
+        $user->teacher_id = $teacher->id;
+        $user->save();
+        */
+        $formData = $request->validated();
+        $photo_path = null;
+
+        if (isset($formData['photo'])) {
+            $photo_path = Storage::put('uploads/images', $formData['photo']);
+        }
         //
+        return redirect()->route('user.dashboard');
     }
 
     /**
