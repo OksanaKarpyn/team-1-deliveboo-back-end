984c8209b864d522f9340403d4d059d66149e244
Oksana Karpyn <oksana101992@gmail.com>
2024-01-29 16:24:41 +0100

    dishSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             RestaurantSeeder::class,
             Typeseeder::class,
+            DishSeeder::class,
             
         ]);
     }
--- database/seeders/DishSeeder.php
+++ database/seeders/DishSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Dish;
+use App\Models\Restaurant;
 
 class DishSeeder extends Seeder
 {
@@ -12,8 +15,32 @@ class DishSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $restaurants = Restaurant::all();
+        $dishes=[
+         "Pasta alla carbonara",
+          "Pizza Margherita",
+          "Lasagne alla bolognese",
+          "Risotto ai funghi", 
+          "Spaghetti alla puttanesca",
+          "Cotoletta alla milanese",
+          "Pollo alla cacciatora",
+          "Bistecca alla fiorentina",
+          "Insalata caprese", 
+          "Tiramisù",
+        ];
+        foreach($restaurants as $restaurantElem){
+            for($i = 0; $i < 5;$i++){
+                $newDishes = new Dish();
+                $newDishes->name = $dishes[$i];
+                $newDishes->restaurant_id = $restaurantElem['id'];
+                $newDishes->description = $faker->text(100);
+                $newDishes->ingredients = $faker->words(5,true);
+                $newDishes->price = $faker->randomFloat(2, 5, 20);
+                $newDishes->save();
+            }
+
+        }
     }
 }
