2b6b87bceefd9c6851a8d381cd517d0331e170ed
Andreapiani95 <andreapiani1995@gmail.com>
2024-01-30 12:08:54 +0100

    commit

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,7 +33,7 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'lastname' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
             'restaurantName' => ['required', 'string', 'max:255'],
             'address' => ['required', 'string', 'max:255'],
