551ff47fe23dd0d1deeeb9dc144c4babde9a93e5
SimonaCaruso87 <caruso.simona87@gmail.com>
2024-01-31 23:07:46 +0100

    Edit FIle Login Blade php

--- resources/views/User/restaurant/index.blade.php
+++ resources/views/User/restaurant/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <section class="container">
-        {{ $restaurant->name }}
+        {{-- {{ $restaurant->name }} --}}
         {{-- <a class="btn btn-success" href="{{ route('user.dish.create') }}">Crea Nuovo Piatto</a> --}}
         <a class="btn btn-success text-white me-3 fs-5 my-5" href="{{ route('user.dish.index') }}">lista piatti</a>
     </section>
