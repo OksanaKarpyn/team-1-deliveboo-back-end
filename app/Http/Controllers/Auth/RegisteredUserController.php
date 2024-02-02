bba444eca00e72b785ea1895115f2e34ce4b705f
LucaZanfrisco <lzanfrisco9@gmail.com>
2024-02-02 11:53:48 +0100

    Remove model import

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\Restaurant;
 use App\Models\User;
-use App\Models\Restaurant;
 use App\Models\Type;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
